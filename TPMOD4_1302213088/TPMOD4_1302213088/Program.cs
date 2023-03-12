8620530dd2eb0d1304406ef916f0634a9c1fe1b8
mamanGod <127042964+mamanGod@users.noreply.github.com>
2023-03-13 00:23:28 +0700

    menambahkan implementasi dengan table-driven

--- TPMOD4_1302213088/TPMOD4_1302213088/Program.cs
+++ TPMOD4_1302213088/TPMOD4_1302213088/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+public class KodePost
+{
+    public enum kelurahan { batununggal, kujangsari, mengger, weters, cijaura, jatisari, margasari, sekajati, kebonwaru, maleer, samoja }
+    public static int getKodePost(kelurahan kel)
+    {
+        int[] kodepost = { 40266, 40287, 40267, 40256, 40287, 40286, 40286, 40286, 40272, 40274, 40273 };
+        return kodepost[(int)kel];
+    }
+}
